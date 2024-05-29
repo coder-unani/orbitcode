0bb73758995f1065b0a090d945bcb528102b08c9
hwakk <97441379+hwakk@users.noreply.github.com>
2024-05-30 01:44:54 +0900

    [feat] 스크롤 이벤트 기능 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { MouseEventHandler, useEffect } from 'react';
 
-const Nav = () => {
+interface NavProps {
+  componentIndex: number;
+  handleClick: (index: number) => void;
+  // toggleIsBreak: () => void;
+}
+
+// const Nav = ({ componentIndex, toggleIsBreak }: NavProps) => {
+const Nav = ({ componentIndex, handleClick }: NavProps) => {
+  // 카테고리 리스트
+  const categories = ['Home', 'Works', 'About', 'Contact', 'Hire us'];
+
+  // setTimeout(() => {
+  //   toggleIsBreak();
+  // }, 800);
+
   return (
     <nav className="nav-content">
       <ul>
-        <li className="active">
-          <span>Home</span>
-        </li>
-        <li>
-          <span>Works</span>
-        </li>
-        <li>
-          <span>About</span>
-        </li>
-        <li>
-          <span>Contact</span>
-        </li>
-        <li>
-          <span>Hire us</span>
-        </li>
+        {categories.map((category, index) => (
+          <li key={index} className={index === componentIndex ? 'active' : ''} onClick={() => handleClick(index)}>
+            <span>{category}</span>
+          </li>
+        ))}
       </ul>
     </nav>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
 import Header from 'components/Header';
 import Nav from 'components/Nav';
 import Intro from 'pages/Intro';
 import Work from 'pages/Work';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import 'styles/Home.css';
 import About from './About';
 import Contact from './Contact';
 import Hire from './Hire';
-import 'styles/Home.css';
 
 /**
  * @TODOS
@@ -15,27 +15,111 @@ import 'styles/Home.css';
  */
 
 const Home = () => {
+  // 컴포넌트 번호
+  const [componentIndex, setComponentIndex] = useState<number>(0);
+
+  // 컴포넌트 리스트
+  const components = [<Intro />, <Work />, <About />, <Contact />, <Hire />];
+
+  /*
+  // 스크롤 중복 방지
+  let isBreak = false;
+
+  const movedown = (e: Event) => {
+    const moveidx = componentIndex === components.length - 1 ? 0 : componentIndex + 1;
+    setTimeout(() => {
+      setComponentIndex(moveidx);
+    }, 800);
+  };
+
+  const moveup = (e: Event) => {
+    const moveidx = componentIndex === 0 ? components.length - 1 : componentIndex - 1;
+    setTimeout(() => {
+      setComponentIndex(moveidx);
+    }, 800);
+  };
+
+  const toggleIsBreak = () => {
+    isBreak = !isBreak;
+  };
+
+  const handleScroll = (e: WheelEvent) => {
+    if (!isBreak) {
+      if (e.deltaY > 0) {
+        movedown(e);
+      } else {
+        moveup(e);
+      }
+      window.removeEventListener('wheel', handleScroll);
+    }
+  };
+
+  // window.addEventListener('touchmove', (e) => {
+  //   //
+  // });
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleScroll);
+    return () => {};
+  }, [componentIndex]);
+  */
+
+  // 스크롤 중복 방지
+  const [isThrottled, setIsThrottled] = useState<boolean>(false);
+
+  const handleNavigation = useCallback(
+    (e: WheelEvent) => {
+      if (isThrottled) {
+        return; // 0.8초 동안은 추가 변경을 막음
+      }
+
+      let { deltaY } = e;
+      let index = componentIndex;
+
+      // 스크롤 방향에 따라 컴포넌트 번호 변경
+      if (deltaY < 0) {
+        // 스크롤 업
+        index = index === 0 ? components.length - 1 : index - 1;
+      } else if (deltaY > 0) {
+        // 스크롤 다운
+        index = index === components.length - 1 ? 0 : index + 1;
+      }
+
+      setComponentIndex(index);
+      // 변경 후 0.8초 동안 추가 변경을 막음
+      setIsThrottled(true);
+
+      setTimeout(() => {
+        // 0.8초 후 추가 변경 가능
+        setIsThrottled(false);
+      }, 800);
+    },
+    [componentIndex, isThrottled],
+  );
+
+  const handleClick = useCallback((index: number): void => {
+    setComponentIndex(index);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleNavigation);
+    return () => {
+      window.removeEventListener('wheel', handleNavigation);
+    };
+  }, [handleNavigation]);
+
   return (
     <div className="wrap">
       <Header />
-      <Nav />
+      {/* <Nav componentIndex={componentIndex} toggleIsBreak={toggleIsBreak} /> */}
+      <Nav componentIndex={componentIndex} handleClick={handleClick} />
       <main className="main-content">
         <ul>
-          <li className="active">
-            <Intro />
-          </li>
-          <li className="active">
-            <Work />
-          </li>
-          <li className="active">
-            <About />
-          </li>
-          <li className="active">
-            <Contact />
-          </li>
-          <li className="active">
-            <Hire />
-          </li>
+          {components.map((component, index) => (
+            <li key={index} className={index === componentIndex ? 'active' : ''}>
+              {component}
+            </li>
+          ))}
         </ul>
       </main>
       <footer></footer>
