a7d9cba6a8b73502eb3f4fe7ee7301d887cb29ed
coder-unani <coder.unani@gmail.com>
2025-05-05 23:39:55 +0900

    [feat] GTM 설치

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'context/theme-context';
+import TagManager from 'react-gtm-module';
 import Main from 'pages/Main';
 import '@fontsource-variable/montserrat';
 import 'remixicon/fonts/remixicon.css';
 import 'styles/Main.css';
 import 'locales/i18n';
+
+// GTM 초기화
+const tagManagerArgs = {
+  gtmId: 'GTM-MKS697RZ',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
